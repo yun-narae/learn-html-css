5ed0c5a35d4fc932c031b5b3e359da94dcdf0d1e
seulbinim <skfo0827@naver.com>
2024-12-20 13:18:49 +0900

    반응형 breakpoint 지정

--- 05-tailwind/tailwind.config.js
+++ 05-tailwind/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: { min: "320px", max: "759px" },
+      md: { min: "760px", max: "1023px" },
+      lg: { min: "1024px" },
+    }
   },
   plugins: [],
 }
