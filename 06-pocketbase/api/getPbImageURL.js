ca8c0118682a5addcea78c9648c0bb98b728a7ef
seulbinim <skfo0827@naver.com>
2024-12-20 19:40:27 +0900

    vite & pocketbase & tailwind 설치

---
+++ 06-pocketbase/api/getPbImageURL.js
@@ -0,0 +1,5 @@
+
+// 이미지 관련
+export default function getPbImageURL(item,fileName = 'photo'){
+    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item[fileName]}`
+  }
---
+++ 06-pocketbase/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+const viteConfig = defineConfig({
+
+    server:{
+        host:'localhost',
+        port:3000,
+        cors:true,
+    }
+
+})
+
+export default viteConfig;
