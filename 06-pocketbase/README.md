5b7da64ef09eebfecb8b56f556ec748366956da7
seulbinim <skfo0827@naver.com>
2024-12-24 11:18:16 +0900

    06-pocketbase reset

--- 06-pocketbase/README.md
+++
@@ -1,1 +0,0 @@
-# pocketbase 공부
--- 06-pocketbase/api/getPbImageURL.js
+++
@@ -1,5 +0,0 @@
-
-// 이미지 관련
-export default function getPbImageURL(item,fileName = 'photo'){
-    return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item[fileName]}`
-  }
