409d82bdc82240963bf7da160bfbfebd2edcfe56
seulbinim <skfo0827@naver.com>
2024-12-30 20:01:21 +0900

    css 경로수정

--- 06-react-vite-pocketbase/src/main.jsx
+++ 06-react-vite-pocketbase/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '/dist/main.css';
+import '/main.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
