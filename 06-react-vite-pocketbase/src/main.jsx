75318c4acedad2c20b31ff3b9943223ab3811291
seulbinim <skfo0827@naver.com>
2024-12-30 19:58:53 +0900

    배포 경로 수정

--- 06-react-vite-pocketbase/src/main.jsx
+++ 06-react-vite-pocketbase/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '/dist/output.css';
+import '/dist/main.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
--- 06-react-vite-pocketbase/vite.config.js
+++ 06-react-vite-pocketbase/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
- 
+import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +12,19 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
- 
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "output.css") {
+            return "output.css"; // 특정 CSS 파일 이름 고정
+          }
+          return "[name].[ext]";
+        },
+      },
+    },
+  },
 });
