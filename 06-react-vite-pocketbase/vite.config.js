0a785efc69b9540a41ffea76c3901518f12d49c3
seulbinim <skfo0827@naver.com>
2024-12-30 19:26:22 +0900

    배포 준비

--- 06-react-vite-pocketbase/vite.config.js
+++ 06-react-vite-pocketbase/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/learn-html-css/06-react-vite-pocketbase/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
